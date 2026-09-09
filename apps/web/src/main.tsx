a894264154414c8c39e8eacfc7f40390010d3284
MarcusGraetsch <marcusgraetsch@gmail.com>
2026-09-09 17:24:35 +0200

    feat: route new assessment action through intake wizard

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { AssessmentIntakeShell } from './AssessmentIntakeShell'
 import './styles.css'
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App/></React.StrictMode>)
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AssessmentIntakeShell><App/></AssessmentIntakeShell></React.StrictMode>)
